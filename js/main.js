b0b22d41cb25f0f3f44ed42d3d5638db2650d2b0
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-02-21 16:02:38 +0100

    inserito variante let const su passWord

--- js/main.js
+++ js/main.js
@@ -16,6 +16,21 @@ const favoriteColor = prompt('Colore preferito?');
 console.log(favoriteColor);
 document.getElementById('favoritecolor').innerHTML = favoriteColor;
 
-const passWord = `${firstName}${lastName}${favoriteColor}${age}`
+/* se uso let popsso dichiararla e poi succesivamente assgnarla mentre con const no, la devo dichiarare ed assegnare */
+/* sostanzialmente non posso mutare il vaolore di const una volta dichiarato */
+
+/*  primo metodo*/
+
+/* 
+const passWord = `${firstName}${lastName}${favoriteColor}${age}`;
 console.log(passWord);
 document.getElementById('password').innerHTML = `Ciao boomer questa è la tua password monouso (si spera) \: ${passWord}`;
+*/
+
+/* secondo metodo  */
+
+let passWord;
+passWord = `${firstName}${lastName}${favoriteColor}${age}`;
+console.log(passWord);
+document.getElementById('password').innerHTML = `Ciao boomer questa è la tua password monouso (si spera) \: ${passWord}`;
+
