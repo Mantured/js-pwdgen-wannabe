c92583829bba7c2dc10a9805da8778ac2cc04f09
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-02-21 17:40:18 +0100

    prove js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-
+/*
 const firstName = prompt('Come ti chiami?');
 console.log(firstName);
 document.getElementById('firstname').innerHTML = firstName;
 
-const lastName = prompt('inserisci il tuo cognome?');
+const lastName = prompt('Inserisci il tuo cognome?');
 console.log(lastName);
 document.getElementById('lastname').innerHTML = lastName;
 
-const birthYear = prompt('in che anno sei nato?'); 
+const birthYear = prompt('In che anno sei nato?');
 let age = 2022 - parseInt(birthYear);
 console.log(birthYear);
 document.getElementById('age').innerHTML = age;
 
-const favoriteColor = prompt('Colore preferito?');
+const favoriteColor = prompt('Il tuo colore preferito?');
 console.log(favoriteColor);
 document.getElementById('favoritecolor').innerHTML = favoriteColor;
+*/
 
 /* se uso let popsso dichiararla e poi succesivamente assgnarla mentre con const no, la devo dichiarare ed assegnare */
 /* sostanzialmente non posso mutare il vaolore di const una volta dichiarato */
@@ -29,8 +30,29 @@ document.getElementById('password').innerHTML = `Ciao boomer questa è la tua pa
 
 /* secondo metodo  */
 
+/* 
 let passWord;
 passWord = `${firstName}${lastName}${favoriteColor}${age}`;
 console.log(passWord);
 document.getElementById('password').innerHTML = `Ciao boomer questa è la tua password monouso (si spera) \: ${passWord}`;
+ */
 
+const firstName = prompt('Inserisci il tuo nome');
+console.log(firstName);
+
+const lastName = prompt('Inserisci il tuo cognome');
+console.log(lastName);
+
+const birthYear = prompt('inserisci l\'anno di nascita');
+let age = 2022 - parseInt(birthYear);
+console.log(birthYear);
+
+const favoriteColor = prompt('Inserisci il tuo colore preferito');
+console.log(favoriteColor);
+
+const passWord = `${firstName}${lastName}${favoriteColor}${age}`;
+console.log(passWord);
+document.getElementById('password').innerHTML = `Ciao ${firstName} ${lastName}! la tua nuova password super segreta è\: ${passWord}`;
+
+/* oppure in questo caso andiamo ad aggiungere il "ciao.." sull'html */ 
+/* document.getElementById('password').innerHTML = firstName + lastName + favoriteColor + age; */
